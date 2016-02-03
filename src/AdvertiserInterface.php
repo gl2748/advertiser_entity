6bcd95dc6802d8b5cafa466f897d58d5418ee4b9
gl2748 <imaitland@gmail.com>
2016-02-03 12:49:26 -0500

    working barebones entity

---
+++ src/AdvertiserInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\advertiser\AdvertiserInterface.
+ */
+
+namespace Drupal\advertiser;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\user\EntityOwnerInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+
+/**
+ * Provides an interface defining a contact message entity.
+ */
+interface AdvertiserInterface extends ContentEntityInterface {
+
+}
---
+++ src/Entity/Advertiser.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\advertiser\Entity\Advertiser.
+ */
+
+namespace Drupal\advertiser\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\advertiser\AdvertiserInterface;
+
+/**
+ * Defines the Advertiser entity.
+ *
+ * @ingroup advertiser 
+ *
+ * @ContentEntityType(
+ *   id = "advertiser",
+ *   label = @Translation("Advertiser"),
+ *   base_table = "advertiser",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "name"
+ *   },
+ * )
+ *
+ */
+
+class Advertiser extends ContentEntityBase implements AdvertiserInterface{
+
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+     // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Advertiser entity.'))
+      ->setReadOnly(TRUE);
+ 
+      
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t("The advertiser's name"))
+      ->setDescription(t('The name of the advertiser.'));
+
+    return $fields;
+  }
+}
