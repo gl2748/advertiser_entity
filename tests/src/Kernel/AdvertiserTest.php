f6af33d8d37068694ad173747ff0f5375c363b4d
Benjamin Chodoroff <bc@thermitic.net>
2016-02-04 17:15:01 -0500

    add a kernel test

---
+++ tests/src/Kernel/AdvertiserTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\advertiser\Kernel\AdvertiserTest.
+ */
+
+namespace Drupal\Tests\advertiser\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+use \Drupal\advertiser\Entity\Advertiser;
+
+/**
+ * @coversDefaultClass \Drupal\advertiser\Entity\Advertiser
+ * @group advertiser
+ */
+class AdvertiserTest extends KernelTestBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static $modules = ['advertiser', 'system'];
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installEntitySchema('advertiser');
+  }
+
+  /**
+   * Gets storage interface for our entity.
+   *
+   * @return \Drupal\Core\Entity\EntityStorageInterface
+   *   A storage interface.
+   */
+  protected function getStorage() {
+    return \Drupal::entityManager()->getStorage('advertiser');
+  }
+
+  /**
+   * Saves an advertiser & make sure values are properly set.
+   */
+  public function testSaveAdvertiser() {
+    $label = 'ADM: Supermarket to the World';
+
+    // Create an entity.
+    $entity = Advertiser::create([
+      'name' => $label,
+    ]);
+
+    // Save it.
+    $entity->save();
+
+    // Get the id.
+    $id = $entity->id();
+
+    // Load the saved entity.
+    $saved_entity = Advertiser::load($id);
+
+    $this->assertEquals($label, $saved_entity->label());
+  }
+
+}
