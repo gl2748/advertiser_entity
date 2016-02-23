e5d96b641b53d72ba252d23143b8d6ca553191b9
gl2748 <imaitland@gmail.com>
2016-02-23 11:10:00 -0500

    Added test for URL length validator

--- tests/src/Kernel/AdvertiserValidationTest.php
+++ tests/src/Kernel/AdvertiserValidationTest.php
@@ -39,22 +39,21 @@ class AdvertiserValidationTest extends KernelTestBase {
    * Runs entity validation checks.
    */
   public function testAdvertiserUrls() {
-    $entity = Advertiser::create([
+    $advertiser = Advertiser::create([
       'advertiser_website' => 'www.testing.com',
     ]);
-    $violations = $entity->validate();
+    $violations = $advertiser->validate();
     $this->assertEqual(count($violations), 0, 'No violations when validating a legit url.');
-    /*
+
     // Only test one example invalid name here, TODO the rest.
-    $longstring = $this->randomMachineName(2084); //Limit is set to 2083 so this is going to be too long.
+    // Limit is set to 2083 so this is going to be too long.
+    $longstring = $this->randomMachineName(2084);
     $longurl = "http://" . $longstring . ".com";
     $advertiser->set('advertiser_website', $longurl);
     $violations = $advertiser->validate();
-
     $this->assertEqual(count($violations), 1, 'Violation found when url is too long.');
     $this->assertEqual($violations[0]->getPropertyPath(), 'advertiser_website');
     $this->assertEqual($violations[0]->getMessage(), t('The url %longurl is too long: it must be %max characters or less.', array('%longurl' => $longurl, '%max' => 2083)));
-     */
   }
 
 }
